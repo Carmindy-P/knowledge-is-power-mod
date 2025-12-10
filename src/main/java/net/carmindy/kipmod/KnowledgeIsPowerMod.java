9aa76ff0bca568ad59ebdbfcba76e6d151e594fc
Carmindy-P <cphillips30@murraystate.edu>
2025-12-09 18:41:22 -0600

    Got it launching.

--- src/main/java/net/carmindy/kipmod/KnowledgeIsPowerMod.java
+++ src/main/java/net/carmindy/kipmod/KnowledgeIsPowerMod.java
@@ -6,14 +6,18 @@ import net.carmindy.kipmod.events.KIPModEvents;
 import net.fabricmc.api.ModInitializer;
 
 public class KnowledgeIsPowerMod implements ModInitializer {
+
     public static final String MOD_ID = "knowledge-is-power-mod";
 
     @Override
     public void onInitialize() {
-        // Register abilities
+        // Register abilities so AbilityRegistry.get("flame") returns the ability
         AbilityRegistry.register("flame", new FlameAbility());
 
-        // Register events (book use handler, etc.)
+        // Register event handlers (book use, etc.)
         KIPModEvents.register();
+
+        // Runtime sanity print (shows up in the game log)
+        System.out.println(MOD_ID + " initialized: abilities and events registered");
     }
 }
